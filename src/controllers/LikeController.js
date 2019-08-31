72461260361ba95b86c5e68cfc0005efe6a116c4
Denner Lemos <dennerlemos91@gmail.com>
2019-08-31 19:27:27 -0300

    Implementação da conexão via websocket para informar devs que deram match

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,12 +2,8 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
-        const {
-            user
-        } = req.headers;
-        const {
-            devId
-        } = req.params;
+        const { user } = req.headers;
+        const { devId } = req.params;
 
         const loggedDev = await Dev.findById(user);
         const targetDev = await Dev.findById(devId);
@@ -15,17 +11,26 @@ module.exports = {
         if (!targetDev) {
             return res.status(400).json({
                 error: 'Dev not exists'
-            })
+            });
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('DEU MACTH')
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
-        loggedDev.likes.push(targetDev._id)
+        loggedDev.likes.push(targetDev._id);
 
         await loggedDev.save();
 
-        return res.json(loggedDev)
+        return res.json(loggedDev);
     }
-}
+};
--- src/server.js
+++ src/server.js
@@ -3,16 +3,34 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
-    'mongodb+srv://omnistack:omnistack@cluster0-pik74.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    'mongodb+srv://omnistack:omnistack@cluster0-pik74.mongodb.net/omnistack8?retryWrites=true&w=majority',
+    {
         useNewUrlParser: true
     }
 );
-server.use(cors());
-server.use(express.json());
 
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+});
 
-server.listen(3333);
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+server.listen(3333, () => {
+    console.log('Server in listem');
+});
